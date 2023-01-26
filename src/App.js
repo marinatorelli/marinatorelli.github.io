be655791b637739b20c0e280da961c006801092b
marinatorelli <marinatorellipostigo@gmail.com>
2023-01-26 23:51:59 +0100

    styling text and overflow

--- src/App.js
+++ src/App.js
@@ -10,19 +10,19 @@ import Pdf from './Documents/MarinaTorelliCV.pdf';
 
 function App() {
   return (
-    <>
-      <div className='container'>
-        <div className='title'>Hi! I'm Marina Torelli</div>
-        <div className='subtitle'>I’m a recent Computer Science and Engineering university graduate</div>
-        <div className='desc'>I have a passion for all things tech and design</div>
-        <div className='resume'><a href={Pdf} target="_blank" rel="noreferrer">my cv</a></div>
-        <div className='icons'>
-          <a className='icon' href="https://www.linkedin.com/in/marina-torelli/" > <FontAwesomeIcon icon={faLinkedin} size="2x" /></a>
-          <a className='icon' href="https://github.com/marinatorelli" > <FontAwesomeIcon icon={faGithub} size="2x" /></a>
-          <a className='icon' href="mailto:marinatorellipostigo@gmail.com" > <FontAwesomeIcon icon={faEnvelope} size="2x" /></a>
-        </div>
-      </div >
-    </>
+
+    <div className='container'>
+      <div className='title text' >Hi, I'm Marina Torelli</div>
+      <div className='subtitle text'>I’m a recent Computer Science and Engineering university graduate</div>
+      <div className='desc text'>I have a passion for all things tech and design</div>
+      <div className='resume'><a className='resume-a' href={Pdf} target="_blank" rel="noreferrer">resume</a></div>
+      <div className='icons'>
+        <a className='icon' href="https://www.linkedin.com/in/marina-torelli/" > <FontAwesomeIcon icon={faLinkedin} size="2x" /></a>
+        <a className='icon' href="https://github.com/marinatorelli" > <FontAwesomeIcon icon={faGithub} size="2x" /></a>
+        <a className='icon' href="mailto:marinatorellipostigo@gmail.com" > <FontAwesomeIcon icon={faEnvelope} size="2x" /></a>
+      </div>
+    </div >
+
   );
 }
 
